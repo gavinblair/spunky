9bba6511e3d23833763b3bde67f168e31dd325ee
gavinblair <gavin.blair@rtraction.com>
2012-10-06 00:41:54 -0400

    works on iphone. added some more pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 window.jQuery(document).ready(function($){
 	setInterval(function(){
-					$('blink').toggleClass('unblink');
-				}, 500);
+		$('blink').toggleClass('unblink');
+	}, 500);
+
+	$(".title").click(function(){
+		window.history.back();
+	});
+
 });
